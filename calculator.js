332e93045b1ca0cf0da822af000cbe4755a71087
sudharshan <kamavaramsudharshan1706@gmail.com>
2022-10-17 10:10:43 +0530

    updated with bundle

--- calculator.js
+++ calculator.js
@@ -12,18 +12,22 @@ for(var btn of btns){
     else if(btn.textContent == "C"){
         btn.setAttribute('onClick','clr()')
         btn1.style.color = 'red'
+        btn1.id = 'clear'
     }
     else if(btn.textContent == "="){
         btn.setAttribute('onClick','solve()')
+        btn1.id = 'equal'
         
     }
     else if(btn.textContent == "+"){
         btn.setAttribute('onClick',"dis('+')")
         btn1.style.color = 'blue'
+        btn1.id = 'add'
     }
     else if(btn.textContent == "-"){
         btn.setAttribute('onClick',"dis('-')")
         btn1.style.color = 'blue'
+        btn1.id = 'subtract'
     }
     else if(btn.textContent == "X"){
         btn.setAttribute('onClick',"dis('*')")
@@ -39,42 +43,64 @@ for(var btn of btns){
     }
     else if(btn.textContent == "1"){
         btn.setAttribute('onClick','dis(1)')
+        btn1.id = '1'
     }
     else if(btn.textContent == "2"){
         btn.setAttribute('onClick','dis(2)')
+        btn1.id = '2'
     }
     else if(btn.textContent == "3"){
         btn.setAttribute('onClick','dis(3)')
+        btn1.id = '3'
     }
     else if(btn.textContent == "4"){
         btn.setAttribute('onClick','dis(4)')
+        btn1.id = '4'
     }
     else if(btn.textContent == "5"){
         btn.setAttribute('onClick','dis(5)')
+        btn1.id = '5'
     }
     else if(btn.textContent == "6"){
         btn.setAttribute('onClick','dis(6)')
+        btn1.id = '6'
     }
     else if(btn.textContent == "7"){
         btn.setAttribute('onClick','dis(7)')
+        btn1.id = '7'
     }
     else if(btn.textContent == "8"){
         btn.setAttribute('onClick','dis(8)')
+        btn1.id = '8'
     }
     else if(btn.textContent == "9"){
         btn.setAttribute('onClick','dis(9)')
+        btn1.id = '9'
     }
     else if(btn.textContent == "0"){
         btn.setAttribute('onClick','dis(0)')
+        btn1.id = '0'
     }
     else if(btn.textContent == "00"){
         btn.setAttribute('onClick',"dis('00')")
+        btn1.id = '00'
     }
     btn.textContent = "";
     btn.append(btn1)
 }
-const textPlain = document.getElementById('displayText')
+const h1Ele = document.createElement('h1');
+h1Ele.id = 'title'
+h1Ele.innerText = 'Calculator'
 
+const description = document.createElement('p');
+description.id = 'description'
+description.innerText = 'Calculating values using Calculator'
+
+document.body.append(h1Ele,description);
+
+
+const textPlain = document.getElementById('displayText')
+textPlain.id = 'result'
 function dis(val) {
     if(visited == "1"){
         if(val != "+" && val != "-" && val != "*" && val != "/"){
